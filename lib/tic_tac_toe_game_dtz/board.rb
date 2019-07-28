ab0e22a6ff9b5dd5b12c55e275759742a8abb6f7
David Toledo <dtoledoz0207@gmail.com>
2019-07-28 13:45:16 -0500

    tests-equals-marks-in-rowANDcolumn-working

--- lib/tic_tac_toe_game_dtz/board.rb
+++ lib/tic_tac_toe_game_dtz/board.rb
@@ -1,6 +1,6 @@
 #module TicTacToeGameDtz
 class Board
-  attr_writer :dimension
+  attr_writer :dimension, :board
 
   def initialize(dimension)
     @dimension = dimension
@@ -22,10 +22,9 @@ class Board
     puts ' '
     @dimension.times { |column_number| print "   #{column_number + 1} " }
     2.times { puts ' ' }
-  
     @board.each_with_index do |row, index|
       print "#{ index + 1 }  "
-      fila.each do |column|
+      row.each do |column|
         print column
         print '    '
       end
@@ -33,10 +32,51 @@ class Board
     end
     puts ' '
   end
-  
+
   def available_positions
     @counter_available_positions > 0 ? true : false
   end
+
+  def occupied_box(row, column)
+    box_content = @board[row - 1][column - 1]
+    box_content == 'X' || box_content == 'O' ? true : false
+  end
+
+  def add_mark(mark, row, column)
+    @counter_available_positions -= 1
+    @board[row - 1][column - 1] = mark
+  end
+
+  def equals_marks_in_row(row)
+    marks_in_row = ''
+    @board[row - 1].each do |column|
+      marks_in_row << column
+    end
+
+    if marks_in_row == @mark_x_winner
+      puts ' ***** ¡PLAYER 1 IS THE WINNER! ***** '
+      true
+    elsif marks_in_row == @mark_o_winner
+      puts ' ***** ¡PLAYER 2 IS THE WINNER! ***** '
+      true
+    end
+  end
+
+  def equals_marks_in_column(column)
+    marks_in_column = ''
+    @board.each do |row|
+      marks_in_column << row[column - 1]
+    end
+
+    if marks_in_column == @mark_x_winner
+      puts ' ***** ¡PLAYER 1 IS THE WINNER! ***** '
+      true
+    elsif marks_in_column == @mark_o_winner
+      puts ' ***** ¡PLAYER 2 IS THE WINNER! ***** '
+      true
+    end
+  end
+
 end
 #end
 
--- spec/tic_tac_toe_game_dtz_spec.rb
+++ spec/tic_tac_toe_game_dtz_spec.rb
@@ -2,17 +2,62 @@ require 'tic_tac_toe_game_dtz/board'
 
 RSpec.describe Board do
   describe '#available_positions' do
-    context 'When there are available positions' do
+    context 'When there are free positions' do
       it 'is true and the game continue' do
         board = Board.new(3)
         expect(board.available_positions).to be true
       end
     end
-    context 'When there are No available positions' do
-      it 'is false and the game is over' do
+    context 'When there are No free positions' do
+      it 'is false and the game is over by draw' do
         board = Board.new(0)
         expect(board.available_positions).to be false
       end
     end
   end
+
+  describe '#occupied_box' do
+    context 'When there is a X or O mark in the box' do
+      it 'is true, the user cannot put a mark in that box' do
+        board = Board.new(3)
+        board.board = [['_','_','_'], ['_','X','_'], ['_','_','_']]
+        row = 2
+        column = 2
+        expect(board.occupied_box(row, column)).to be true
+      end
+    end
+    context 'When there is NO a X or O mark in the box' do
+      it 'is false, the user can put a mark in that box' do
+        board = Board.new(3)
+        board.board = [['_','_','_'], ['_','_','_'], ['_','_','_']]
+        row = 2
+        column = 2
+        expect(board.occupied_box(row, column)).to be false
+      end
+    end
+  end
+
+  describe '#equals_marks_in_row' do
+    context 'When there are three equals player marks in a row' do
+      it 'is true and one player win' do
+        board = Board.new(3)
+        board.board = [['O','O','O'], ['_','_','_'], ['_','_','_']]
+        row = 1
+        expect(board.equals_marks_in_row(row)).to be true
+      end
+    end
+  end
+
+  describe '#equals_marks_in_column' do
+    context 'When there are three equals player marks in a column' do
+      it 'is true and one player win' do
+        board = Board.new(3)
+        board.board = [['X','_','_'], ['X','_','_'], ['X','_','_']]
+        column = 1
+        expect(board.equals_marks_in_column(column)).to be true
+      end
+    end
+  end
+
+
 end
